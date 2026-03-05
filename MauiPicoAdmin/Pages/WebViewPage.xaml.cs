52fc46cd12212d03da4cd93c6bd90e9a3fdb7e98
Alex chow <zhouchuanglin@gmail.com>
2026-03-05 16:01:42 +0100

    WebView 壳模式

---
+++ MauiPicoAdmin/Pages/WebViewPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace MauiPicoAdmin.Pages;
+
+public partial class WebViewPage : ContentPage
+{
+    public WebViewPage()
+    {
+        InitializeComponent();
+    }
+}
